123c3f879c32b38ac600a215d19c7803bee63d64
Marea Norton <nortonmarea@gmail.com>
2022-02-15 11:48:12 +1030

    second commit #8

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+      <php>Sit laborum irure aliquip aliqua est magna sunt cillum.
+      Enim velit laboris laborum occaecat ex ut occaecat elit. 
+      Fugiat et adipisicing irure aliqua elit nisi labore nulla amet. 
+      Voluptate sunt esse amet nisi id. Ullamco laboris deserunt in aliqua 
+      sunt deserunt sit fugiat eiusmod. Ipsum voluptate reprehenderit deserunt 
+      est consequat labore commodo. Ex nulla voluptate minim dolore labore id Lorem eiusmod cillum Lorem.
+      </php>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page</h1>
+      <php>Sit laborum irure aliquip aliqua est magna sunt cillum.
+      Enim velit laboris laborum occaecat ex ut occaecat elit. 
+      Fugiat et adipisicing irure aliqua elit nisi labore nulla amet. 
+      Voluptate sunt esse amet nisi id. Ullamco laboris deserunt in aliqua 
+      sunt deserunt sit fugiat eiusmod. Ipsum voluptate reprehenderit deserunt 
+      est consequat labore commodo. Ex nulla voluptate minim dolore labore id Lorem eiusmod cillum Lorem.
+      </php>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
